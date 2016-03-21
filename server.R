f4566b81332fba078aae337258bd714f8a2f717c
Brewstarke <afstarke@gmail.com>
2016-03-21 08:07:43 -0400

    inital Commit

---
+++ server.R
@@ -0,0 +1,91 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(htmltools)
+library(shinyapps)
+library(streamgraph)
+library(dplyr)
+library(tidyr)
+library(rebird)
+library(magrittr)
+library(leaflet)
+library(rgdal)
+library(dplyr)
+library(tidyr)
+library(readr)
+
+shinyServer(function(input, output) {
+
+	
+	# create dataframe from loaded data for use in app 
+	# Move this to an intro tab page with instructions:
+	eBird_filedata <- reactive({
+		req(input$ebirdData)
+		infile <- input$ebirdData
+		data <- read.csv(infile$datapath, header = TRUE)
+		data 
+	})
+	
+		output$testText <- renderText({
+			dim(eBird_filedata())
+		    })
+	
+
+	
+	# Map 1 ----
+	# Points of all observations across globe.
+	# Use same methods as superzip- make blank map then use leafletProxy to add data.
+
+		output$eBirdMap <- renderLeaflet({
+			req(input$ebirdData)
+			leaflet() %>% 
+			addProviderTiles("CartoDB.Positron") %>% 
+				setView(lng = 5, lat = 5, zoom = 2)
+		})
+		
+		# Hopefully this fixes the points not loading on the map on tab #2
+		outputOptions(output, 'eBirdMap', priority = 1)
+	
+	observe({
+		leafletProxy('eBirdMap') %>% 
+			clearShapes() %>% 
+			addCircleMarkers(data = eBird_filedata(), 
+					 lng = ~Longitude, 
+					 lat = ~Latitude,
+					 stroke = FALSE, 
+					 fillOpacity = .8, 
+					 color = "#226666", 
+					 radius = 3, 
+					 popup = ~Common.Name,
+					 layerId = ~Submission.ID)
+		
+	})
+	
+	# Display list of common names from each submission on click of map. Nearly all this borrowed form SuperZip app.
+	# showSpeciesListPopup <- function(listID, lat, lon){
+	# 	selectedSubmission <- mapData()[mapdata()$Submission.ID == listID,]
+	# 	content <- as.character()
+	# }
+	# 
+	# clickList <- reactive(
+	# 	click <- input$ebirdMap_marker_click
+	# )
+	# 
+	# output$speciesList <- renderDataTable(
+	# 	if(is.null(input$ebirdData)){
+	# 		return(NULL)
+	# 	}
+	# 	
+	# 	SpeciesList <- function(input$ebirdMap_marker_click){
+	# 		list <- data %>% unnest() %>% select(Common.Name) %>% datatable()
+	# 		list
+	# 	}
+	# )
+	
+	
+})
---
+++ ui.R
@@ -0,0 +1,51 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(htmltools)
+library(shinyapps)
+library(streamgraph)
+library(dplyr)
+library(tidyr)
+library(rebird)
+library(magrittr)
+library(leaflet)
+library(rgdal)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(DT)
+
+
+shinyUI(navbarPage("eBird Personal Observations Explorer", 
+	tabPanel("Observation Mapper",
+	 leafletOutput(outputId = "eBirdMap", width = '100%', height = 800),
+	# Sidebar with a slider input for number of bins 
+	absolutePanel(id = "controls", fixed = TRUE,
+		      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+		      width = 250, height = "auto",
+		      h3('Load in you eBird data:'),
+		      fileInput("ebirdData",
+		      	  "Select your eBird data file:",
+		      	  width = 250),
+		      textOutput('testText'))
+	), 
+	# Data Loading and Intro to the app:
+	tabPanel("Personal eBird Explorer",
+		 fluidPage(
+		 	fluidRow(
+		 		column(12,
+		 		       h3("eBird Data Explorer"),
+		 		       p('Looking for a snapshot of your "birding" life? Load in your eBird personal data and lets begin')
+		 		)
+		 		       
+		 	)
+		 )
+	)
+))
+
+
